6a8bdaa1d3c1724f0dbcc204d8278dbd5265b4fb
Palaniguna-123 <gunaguna20994@gmail.com>
2026-08-11 22:09:34 +0530

    changes in colour pallete

--- src/data/index.ts
+++ src/data/index.ts
@@ -1,6 +1,7 @@
 export * from './brand';
 export * from './products';
 export * from './shades';
+export * from './scenes';
 export * from './services';
 export * from './navigation';
 export * from '../types';
